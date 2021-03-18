4ad71255ac088f57e6e81ed4885982f171d02899
cyyjs <cyy0418@qq.com>
2021-03-18 16:33:40 +0800

    update ts declaration file

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 const fileName = process.type === 'browser' ? './server' : './client'
 const Router = require(fileName)
 interface RouterInterface {
-  get (path:string, data:object): Promise<any>
-  put (path:string, data:object): Promise<any>
-  post (path:string, data:object): Promise<any>
-  delete (path:string, data:object): Promise<any>
+  get (path:string, data?:object | Function): Promise<any>
+  put (path:string, data?:object | Function): Promise<any>
+  post (path:string, data?:object | Function): Promise<any>
+  delete (path:string, data?:object|Function): Promise<any>
 }
 const router = new Router() as RouterInterface
 export default router
